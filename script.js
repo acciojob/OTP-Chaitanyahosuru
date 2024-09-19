ed4a91441e17b8d54ef377a6e6b94e824ffdb252
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-09-19 09:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //your JS code here. If required.
-const codes = document.querySelectorAll('.code');
+ <div class="code-container">
+ <input type="text" maxlength="1" class="code" id="digit1" autofocus
+>
+ <input type="text" maxlength="1" class="code" id="digit2">
+ <input type="text" maxlength="1" class="code" id="digit3">
+ <input type="text" maxlength="1" class="code" id="digit4">
+ <input type="text" maxlength="1" class="code" id="digit5">
+ <input type="text" maxlength="1" class="code" id="digit6">
+ </div>
+ <p class="disclaimer">This is design only. We didn't actually send you
+an email as we don't have your email, right?</p>
+ </div>
 
-codes[0].focus();
-
-codes.forEach((code, index) => {
-    const codeId = code-${index + 1};
-    code.addEventListener('keydown', (e) => {
-        if (e.key === 'Backspace' && code.value === '' && index !== 0) {
-            document.getElementById(code-${index}).focus();
-		}
-        }
-    });
