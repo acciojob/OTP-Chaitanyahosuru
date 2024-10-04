49d2feece5cbfac88ec443a152048883c2d67695
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-10-04 18:33:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const codeInputs = document.querySelectorAll('.code');
+
+codeInputs.forEach((input, index) => {
+    input.addEventListener('keyup', (e) => {
+        if (e.target.value.length === 1 && index < codeInputs.length - 1) {
+            codeInputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && index > 0 && e.target.value.length === 0) {
+            codeInputs[index - 1].focus();
+        }
+    });
+});
